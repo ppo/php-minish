1f2427378ba48d55ef5a539ec8d5f5760826b137
Pascal Polleunus <ppolleunus@gmail.com>
2020-04-07 20:28:38 +0200

    Review bin scripts.
    - Define `PRIVATE_DIR` in `.env`.
    - Prefix our env vars.

--- skeleton/bin/generate-sitemap.php
+++ skeleton/bin/generate-sitemap.php
@@ -1,7 +1,23 @@
 #!/usr/bin/env php
 <?php
 define("PUBLIC_DIR", dirname(__DIR__));
-define("PRIVATE_DIR", PUBLIC_DIR . "/_private");
+
+$PRIVATE_DIR = PUBLIC_DIR . "/_private";
+if (!file_exists($PRIVATE_DIR)) {
+  $PRIVATE_DIR = getenv("MINISH_PRIVATE_DIR");
+
+  $envFile = __DIR__ . ".env";
+  if (!$PRIVATE_DIR && file_exists($envFile)) {
+    $env = parse_ini_file($envFile);
+    $PRIVATE_DIR = $env["MINISH_PRIVATE_DIR"];
+  }
+
+  if (!file_exists($PRIVATE_DIR)) {
+    throw new Exception("Private folder not defined.");
+  }
+}
+
+define("PRIVATE_DIR", realpath($PRIVATE_DIR));
 
 require_once PRIVATE_DIR . "/minish.php";
 
