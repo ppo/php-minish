65ff467ca530638d96d47468cff33d755ab968d4
Pascal Polleunus <ppolleunus@gmail.com>
2020-04-13 15:27:36 +0200

    Add example of extra params in routes config.

--- skeleton/_private/config/routes.php
+++ skeleton/_private/config/routes.php
@@ -3,5 +3,8 @@ return [
   "home" => [
     "path" => "/",
     "title" => false,
+    // Whatever you want/need…
+    "description" => "Page description for meta[content] for example.",
+    "subRoutes" => ["route-name", "foo", "bar"],  // For section navigation.
   ],
 ];
--- skeleton/_private/templates/_base.php
+++ skeleton/_private/templates/_base.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $_metaTitle; ?></title>
     <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <?php if ($_routeConfig["description"]): ?>
+      <meta name="description" content="<?php echo $_routeConfig["description"]; ?>">
+    <?php endif; ?>
     <link rel="stylesheet" href="/static/main.css">
   </head>
   <body>
