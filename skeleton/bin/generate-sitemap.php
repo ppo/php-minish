79e42dd26f8558f9b0b2831fd5d3b639df2f1a44
Pascal Polleunus <ppolleunus@gmail.com>
2020-05-18 02:58:14 +0200

    Improve command output.

--- skeleton/bin/generate-sitemap.php
+++ skeleton/bin/generate-sitemap.php
@@ -25,6 +25,6 @@ require_once PRIVATE_DIR . "/minish.php";
 $app = new App(true);
 [$sitemapPath, $googlePingUrl] = $app->generateSitemap();
 
-echo "Sitemap successfully generated: {$sitemapPath}\n\n" .
+echo "\n\e[32mSitemap successfully generated:\e[0m {$sitemapPath}\n\n" .
   "When it's online, you can alert Google that the sitemap has been updated" .
-  "by opening the following URL:\n  curl $googlePingUrl";
+  "by opening the following URL:\n  \e[1;37mcurl $googlePingUrl\e[0m\n\n";
