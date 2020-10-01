51aa250aafbc25e56bfb2f428194fdd7507161b1
Pascal Polleunus <ppolleunus@gmail.com>
2020-10-01 03:53:26 +0200

    Make PRIVATE_DIR hardcoded.

--- skeleton/bin/generate-sitemap.php
+++ skeleton/bin/generate-sitemap.php
@@ -1,23 +1,7 @@
 #!/usr/bin/env php
 <?php
 define("PUBLIC_DIR", dirname(__DIR__));
-
-$PRIVATE_DIR = PUBLIC_DIR . "/_private";
-if (!file_exists($PRIVATE_DIR)) {
-  $PRIVATE_DIR = getenv("MINISH_PRIVATE_DIR");
-
-  $envFile = __DIR__ . ".env";
-  if (!$PRIVATE_DIR && file_exists($envFile)) {
-    $env = parse_ini_file($envFile);
-    $PRIVATE_DIR = $env["MINISH_PRIVATE_DIR"];
-  }
-
-  if (!file_exists($PRIVATE_DIR)) {
-    throw new Exception("Private folder not defined.");
-  }
-}
-
-define("PRIVATE_DIR", realpath($PRIVATE_DIR));
+define("PRIVATE_DIR", PUBLIC_DIR . "/_private");
 
 require_once PRIVATE_DIR . "/minish.php";
 
--- skeleton/index.php
+++ skeleton/index.php
@@ -1,6 +1,6 @@
 <?php
-define("PRIVATE_DIR", __DIR__ . "/_private");
 define("PUBLIC_DIR", __DIR__);
+define("PRIVATE_DIR", PUBLIC_DIR . "/_private");
 
 require_once PRIVATE_DIR . "/minish.php";
 
